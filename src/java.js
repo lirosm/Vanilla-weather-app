28ac762952020714ebcf3005a2fde3ee61394246
Liridona <liridonaosm@gmail.com>
2021-04-12 20:41:54 +0200

    updated URL link

--- src/java.js
+++ src/java.js
@@ -1,7 +1,5 @@
 let apiKey = "c898564bae75feba5b1af523cf4a1211";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
-q=Oslo&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c898564bae75feba5b1af523cf4a1211${apiKey}`;
 
 console.log(apiUrl);
-
 
