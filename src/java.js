fa5b1cc9941b3c5bbc17daf35635620420db6f1e
Liridona <liridonaosm@gmail.com>
2021-04-13 12:43:47 +0200

    updated the time

--- src/java.js
+++ src/java.js
@@ -1,3 +1,38 @@
+function formatDate(timestamp) {
+let date = new Date (timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+hours = `0${hours}`;
+}
+
+let minutes = date.getMinutes();
+if (minutes < 10){
+minutes = `0${minutes}`;
+}
+
+let days = [
+     "Sun",
+     "Mon", 
+     "Tue", 
+     "Wed", 
+     "thu", 
+     "fri", 
+     "Sat"
+    ];
+
+let day = days [date.getDay()];
+
+return `${day} ${hours}:${minutes}`;
+}
+
+
+
+
+
+
+
+
+
 function displayTemp(response){
 console.log(response.data);
 
@@ -7,13 +42,14 @@ let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let feelsLikeElement = document.querySelector("#feels-like");
 let windElement = document.querySelector("#wind");
+let dateElement = document.querySelector("#date");
 temperatureElement.innerHTML = Math.round (response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 feelsLikeElement.innerHTML = Math.round (response.data.main.feels_like);
 windElement.innerHTML = Math.round (response.data.wind.speed);
-
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 
