6ce1c4346029dd9fd0f002782c6bb39b3f934157
Liridona <liridonaosm@gmail.com>
2021-04-13 12:11:35 +0200

    showing the current temp and descirptions

--- src/java.js
+++ src/java.js
@@ -1,5 +1,30 @@
+function displayTemp(response){
+console.log(response.data);
+
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let feelsLikeElement = document.querySelector("#feels-like");
+let windElement = document.querySelector("#wind");
+temperatureElement.innerHTML = Math.round (response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+feelsLikeElement.innerHTML = Math.round (response.data.main.feels_like);
+windElement.innerHTML = Math.round (response.data.wind.speed);
+
+}
+
+
+
+
+
+
+
+
 let apiKey = "c898564bae75feba5b1af523cf4a1211";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c898564bae75feba5b1af523cf4a1211${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Oslo&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
 
+axios.get(apiUrl).then(displayTemp);
