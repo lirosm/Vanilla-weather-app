d7e1001125a3b540f8a159e310993837147468d1
Liridona <liridonaosm@gmail.com>
2021-04-18 15:01:24 +0200

    done visual changes

--- src/java.js
+++ src/java.js
@@ -30,13 +30,13 @@ function formatDay(timestamp) {
 let date = new Date(timestamp * 1000);
 let day = date.getDay();
 let days = [
-     "Sun",
-     "Mon", 
-     "Tue", 
-     "Wed", 
-     "Thu", 
-     "Fri", 
-     "Sat"
+     "SUN",
+     "MON", 
+     "TUE", 
+     "WED", 
+     "THU", 
+     "FRI", 
+     "SAT"
     ];
 
 
